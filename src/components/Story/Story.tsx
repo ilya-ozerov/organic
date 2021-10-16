e68378f53816deaa7f1957c157a30fc2a4d2f4b3
Ilya Ozerov <efwe456lpzx>
2021-10-16 12:12:13 +0300

    added story component with styles

---
+++ src/components/Story/Story.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Story.scss';
+
+import smoothie from '../../assets/images/story/smoothie.png';
+import label from '../../assets/images/story/label.png';
+
+
+export const Story: React.FC = () => {
+    return (
+        <section className="story">
+            <div className="story__row">
+                <div className="story__body">
+                    <div className="story__content">
+                        <div className="story__title title">
+                            <h2>Our story</h2>
+                        </div>
+                        <div className="story__text">
+                            As products typically using raw fruits or vegetables, smoothies include dietary fiber (e.g.
+                            pulp, skin, and seeds) and so are thicker than fruit juice, often with a consistency similar
+                            to
+                            a milkshake.
+                        </div>
+                        <div className="story__button button">
+                            <button>Who we are</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="story__image">
+                    <div className="story__content">
+                        <div className="story__label">
+                            <p>Our ingredients <br/>
+                                are easy to read <br/>
+                                and easier to eat</p>
+                            <img src={label} alt=""/>
+                        </div>
+                        <img src={smoothie} alt="smoothie"/></div>
+                </div>
+            </div>
+        </section>
+    );
+}
