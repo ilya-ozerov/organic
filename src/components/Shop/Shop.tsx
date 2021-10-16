72922eafbcd2d2623feff8ae64d9c9039581f3f5
Ilya Ozerov <efwe456lpzx>
2021-10-16 12:11:55 +0300

    added shop component with styles

---
+++ src/components/Shop/Shop.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './Shop.scss';
+
+import smoothie1 from '../../assets/images/shop/smoothie1.png';
+import smoothie2 from '../../assets/images/shop/smoothie2.png';
+import smoothie3 from '../../assets/images/shop/smoothie3.png';
+import smoothie4 from '../../assets/images/shop/smoothie4.png';
+
+export const Shop: React.FC = () => {
+    return (
+        <section className="shop">
+            <div className="shop__gallery">
+                <div className="shop__item item">
+                    <div className="item__image">
+                        <img src={smoothie1} alt="item"/>
+                    </div>
+                    <div className="item__name">Dark Chocolate Smoothie</div>
+                    <div className="item__label">100 ml</div>
+                    <div className="item__button button">
+                        <button>Buy</button>
+                    </div>
+                </div>
+                <div className="shop__item item">
+                    <div className="item__image">
+                        <img src={smoothie2} alt="item"/>
+                    </div>
+                    <div className="item__name">Dark Chocolate Smoothie</div>
+                    <div className="item__label">100 ml</div>
+                    <div className="item__button button">
+                        <button>Buy</button>
+                    </div>
+                </div>
+                <div className="shop__item item">
+                    <div className="item__image">
+                        <img src={smoothie3} alt="item"/>
+                    </div>
+                    <div className="item__name">Dark Chocolate Smoothie</div>
+                    <div className="item__label">100 ml</div>
+                    <div className="item__button button">
+                        <button>Buy</button>
+                    </div>
+                </div>
+                <div className="shop__item item">
+                    <div className="item__image">
+                        <img src={smoothie4} alt="item"/>
+                    </div>
+                    <div className="item__name">Dark Chocolate Smoothie</div>
+                    <div className="item__label">100 ml</div>
+                    <div className="item__button button">
+                        <button>Buy</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
