dcbbbd939a7692442e7f320c8c095b9f39b8e9d2
Ilya Ozerov <efwe456lpzx>
2021-10-18 14:51:22 +0300

    added adaptive styles of Testimonials component

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -15,29 +15,22 @@ export const Testimonials: React.FC = () => {
 
                 <div className="testimonials__comments">
                     <div className="testimonials__item">
-
                         <div className="testimonials__background">
                             <div className="testimonials__name">Sarah A</div>
                         </div>
-
-                        {/*<div className="testimonials__content">*/}
-                            <div className="testimonials__body">
-                                <div className="testimonials__text">
-                                    Smoothie is great. I don't know what else to say.
-                                    I will let my mum know about this, she
-                                    could really make use of Smoothie! Smoothie saved my business.
-                                </div>
-                                <div className="testimonials__address">
-                                    - from Santa Monica, CA
-                                </div>
+                        <div className="testimonials__body">
+                            <div className="testimonials__text">
+                                Smoothie is great. I don't know what else to say.
+                                I will let my mum know about this, she
+                                could really make use of Smoothie! Smoothie saved my business.
                             </div>
-
-
-                            <div className="testimonials__photo">
-                                <img src={userPhoto} alt="author"/>
+                            <div className="testimonials__address">
+                                - from Santa Monica, CA
                             </div>
-                        {/*</div>*/}
-
+                        </div>
+                        <div className="testimonials__photo">
+                            <img src={userPhoto} alt="author"/>
+                        </div>
                     </div>
                 </div>
 
