77b231da5e8af8ef404c39edab7b5d66a7b2ae7b
Ilya Ozerov <efwe456lpzx>
2021-10-18 16:02:06 +0300

    added burger adaptive styles of Home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -24,26 +24,64 @@ export const Home: React.FC = () => {
 
     return (
         <section className="home">
-            <div className="home__bg">
-                <img draggable={"false"} src={bg} alt="bg"/>
-            </div>
-            <div className="home__body">
+            <div className="home__container">
+                <div className="home__bg">
+                    <img draggable={"false"} src={bg} alt="bg"/>
+                </div>
+                <div className="home__body">
 
-                <header className="home__header header">
+                    <header className="home__header header">
 
-                    {!isBurger &&
-                    <div onClick={() => toggleBurger(true)} className="header__burger">
-                        <div></div>
-                    </div>
-                    }
+                        {!isBurger &&
+                        <div onClick={() => toggleBurger(true)} className="header__burger">
+                            <div></div>
+                        </div>
+                        }
 
-                    {
-                        isBurger &&
-                        <div className="header__mobile mobile">
+                        {
+                            isBurger &&
+                            <div className="header__mobile mobile">
 
-                            <div onClick={() => toggleBurger(false)} className="mobile__cross"></div>
+                                <div onClick={() => toggleBurger(false)} className="mobile__cross"></div>
 
-                            <ul className="mobile__list">
+                                <ul className="mobile__list">
+                                    <li>Home</li>
+                                    <li>Shop</li>
+                                    <li>Story</li>
+                                    <li>About</li>
+                                    <li>Blog</li>
+                                    <li>Testimonials</li>
+                                </ul>
+
+                                <div className="mobile__services">
+                                    <div className="mobile__service">
+                                        <img draggable={"false"} src={search} alt="facebook icon"/>
+                                    </div>
+                                    <div className="mobile__service">
+                                        <img draggable={"false"} src={user} alt="facebook icon"/>
+                                    </div>
+                                    <div className="mobile__service">
+                                        <img draggable={"false"} src={cart} alt="facebook icon"/>
+                                    </div>
+                                </div>
+
+                                <div className="mobile__socials">
+                                    <div className="mobile__social">
+                                        <img draggable={"false"} src={facebook} alt="facebook icon"/>
+                                    </div>
+                                    <div className="mobile__social">
+                                        <img draggable={"false"} src={pinterest} alt="pinterest icon"/>
+                                    </div>
+                                    <div className="mobile__social">
+                                        <img draggable={"false"} src={instagram} alt="instagram icon"/>
+                                    </div>
+                                </div>
+
+                            </div>
+                        }
+
+                        <div className="header__menu">
+                            <ul className="header__list">
                                 <li>Home</li>
                                 <li>Shop</li>
                                 <li>Story</li>
@@ -51,64 +89,40 @@ export const Home: React.FC = () => {
                                 <li>Blog</li>
                                 <li>Testimonials</li>
                             </ul>
+                        </div>
+                        <div className="header__logo logo">
+                            Organic<span>fresh foods</span>
+                        </div>
 
-                            <div className="mobile__socials">
-                                <div className="mobile__social">
+                        <div className="header__panel">
+                            <div className="header__services">
+                                <div className="header__service-item">
+                                    <img draggable={"false"} src={search} alt="facebook icon"/>
+                                </div>
+                                <div className="header__service-item">
+                                    <img draggable={"false"} src={user} alt="facebook icon"/>
+                                </div>
+                                <div className="header__service-item">
+                                    <img draggable={"false"} src={cart} alt="facebook icon"/>
+                                </div>
+                            </div>
+                            <div className="header__socials">
+                                <div className="header__social-item">
                                     <img draggable={"false"} src={facebook} alt="facebook icon"/>
                                 </div>
-                                <div className="mobile__social">
+                                <div className="header__social-item">
                                     <img draggable={"false"} src={pinterest} alt="pinterest icon"/>
                                 </div>
-                                <div className="mobile__social">
+                                <div className="header__social-item">
                                     <img draggable={"false"} src={instagram} alt="instagram icon"/>
                                 </div>
                             </div>
-
                         </div>
-                    }
+                    </header>
 
-                    <div className="header__menu">
-                        <ul className="header__list">
-                            <li>Home</li>
-                            <li>Shop</li>
-                            <li>Story</li>
-                            <li>About</li>
-                            <li>Blog</li>
-                            <li>Testimonials</li>
-                        </ul>
+                    <div className="home__button button">
+                        <button>Shop now</button>
                     </div>
-                    <div className="header__logo logo">
-                        Organic<span>fresh foods</span>
-                    </div>
-
-                    <div className="header__panel">
-                        <div className="header__services">
-                            <div className="header__service-item">
-                                <img draggable={"false"} src={search} alt="facebook icon"/>
-                            </div>
-                            <div className="header__service-item">
-                                <img draggable={"false"} src={user} alt="facebook icon"/>
-                            </div>
-                            <div className="header__service-item">
-                                <img draggable={"false"} src={cart} alt="facebook icon"/>
-                            </div>
-                        </div>
-                        <div className="header__socials">
-                            <div className="header__social-item">
-                                <img draggable={"false"} src={facebook} alt="facebook icon"/>
-                            </div>
-                            <div className="header__social-item">
-                                <img draggable={"false"} src={pinterest} alt="pinterest icon"/>
-                            </div>
-                            <div className="header__social-item">
-                                <img draggable={"false"} src={instagram} alt="instagram icon"/>
-                            </div>
-                        </div>
-                    </div>
-                </header>
-
-                <div className="home__button button">
-                    <button>Shop now</button>
                 </div>
             </div>
         </section>
