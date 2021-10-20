dcf6a1f00b1c4c67dec41dbcddd19e967a203a1c
Ilya Ozerov <efwe456lpzx>
2021-10-20 21:35:11 +0300

    Deleted comments from Testimonials

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import './Testimonials.scss';
 
-import userPhoto from '../../assets/images/testimonials/userPhoto.png'
-import left from '../../assets/images/testimonials/arrowLeft.png'
-import right from '../../assets/images/testimonials/arrowRight.png'
 import {CommentsCarousel} from "./Carousel/CommentsCarousel";
 
 type TestimonialsProps = {
@@ -19,28 +16,6 @@ export const Testimonials: React.FC<TestimonialsProps> = ({testimonialsRef}) => 
                 </div>
 
                 <CommentsCarousel />
-
-                {/*<div className="testimonials__comments">*/}
-                {/*    <div className="testimonials__item">*/}
-                {/*        <div className="testimonials__background">*/}
-                {/*            <div className="testimonials__name">Sarah A</div>*/}
-                {/*        </div>*/}
-                {/*        <div className="testimonials__body">*/}
-                {/*            <div className="testimonials__text">*/}
-                {/*                Smoothie is great. I don't know what else to say.*/}
-                {/*                I will let my mum know about this, she*/}
-                {/*                could really make use of Smoothie! Smoothie saved my business.*/}
-                {/*            </div>*/}
-                {/*            <div className="testimonials__address">*/}
-                {/*                - from Santa Monica, CA*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-                {/*        <div className="testimonials__photo">*/}
-                {/*            <img src={userPhoto} alt="author"/>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-
 
 
             </div>
