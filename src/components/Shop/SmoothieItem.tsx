e49cec5a00e172d5d715a65720931d1b2de18dc7
Ilya Ozerov <efwe456lpzx>
2021-10-21 12:07:45 +0300

    Added cart options to SmoothieItem

--- src/components/Shop/SmoothieItem.tsx
+++ src/components/Shop/SmoothieItem.tsx
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.scss';
-import {BottleOfSmoothie} from "../../types/types";
-import {useDispatch} from "react-redux";
-import {addProduct} from "../../redux/cartReducer";
+import { BottleOfSmoothie } from "../../types/types";
+import { useDispatch, useSelector } from "react-redux";
+import { addProduct, deleteProduct } from "../../redux/cartReducer";
+import { selectProducts } from '../../redux/cartSelectors';
+import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
 
 type SmoothieItemProps = {
     smoothie: BottleOfSmoothie,
 }
 
-export const SmoothieItem: React.FC<SmoothieItemProps> = ({smoothie}) => {
+export const SmoothieItem: React.FC<SmoothieItemProps> = ({ smoothie }) => {
 
     const dispatch = useDispatch();
 
+    const products = useSelector(selectProducts);
+
+    const [currentProductIndex, setCurrentProductIndex] = useState(products.findIndex(
+        p => p.product.id === smoothie.id)
+    )
+
+    useEffect(() => {
+        setCurrentProductIndex(products.findIndex(
+            p => p.product.id === smoothie.id)
+        )
+    }, [products])
+
+    const increaseAmount = () => {
+        dispatch(addProduct({
+            product: products[currentProductIndex].product,
+            amount: products[currentProductIndex].amount,
+        }))
+    }
+
+    const decreaseAmount = () => {
+        dispatch(deleteProduct(smoothie.id))
+    }
+
     const addToCard = () => {
-      dispatch(addProduct({
-          product: smoothie,
-          amount: 1,
-      }))
+        dispatch(addProduct({
+            product: smoothie,
+            amount: 1,
+        }))
+    }
+
+    const productInCart = (): boolean => {
+        const index = products.findIndex(p => p.product.id === smoothie.id)
+
+        if (index === -1) return false;
+
+        return true;
     }
 
     const itemStyle: React.CSSProperties = {
@@ -26,13 +59,26 @@ export const SmoothieItem: React.FC<SmoothieItemProps> = ({smoothie}) => {
     return (
         <div style={itemStyle} className="shop__item item">
             <div className="item__image">
-                <img src={smoothie.imageUrl} alt={smoothie.imageAlt}/>
+                <img src={smoothie.imageUrl} alt={smoothie.imageAlt} />
             </div>
             <div className="item__name">{smoothie.title}</div>
             <div className="item__label">{smoothie.volume}</div>
-            <div className="item__button button">
-                <button onClick={addToCard}>Buy</button>
-            </div>
+
+            {
+                productInCart() &&
+                <div className="item__amount">
+                    <PlusOutlined onClick={increaseAmount} className="item__plus" />
+                    <span>{products[currentProductIndex]?.amount}</span>
+                    <MinusOutlined onClick={decreaseAmount} className="item__minus" />
+                </div>
+            }
+
+            {
+                !productInCart() &&
+                <div className="item__button button">
+                    <button onClick={addToCard}>Buy</button>
+                </div>
+            }
         </div>
     );
 }
